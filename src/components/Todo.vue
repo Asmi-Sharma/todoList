0b7ddf36f9209df83e10863e75dcfc8bf7c93148
Asmi-Sharma <asmi4227@gmail.com>
2021-03-27 17:04:32 +0545

    delete task done

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,23 +1,28 @@
-
-
 <template>
+<<<<<<< Updated upstream
   <div class="list">
     <h1> {{ msg }}</h1>
     <input type="text" class="list" placeholder="Task to add" v-model="newTodo" @keyup.enter="addTodo">
     <div v-for="todo in todos" :key="todo.id" class="todo-item">
       {{todo.title}}
+=======
+  <div class="todo">
+    <input type="text" class="todo-input" placeholder="Task to add" v-model="newTodo" @keyup.enter="addTodo">
+    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+      <div class="todo-item-left">
+        <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label">{{todo.title}}</div>
+        <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" v-focus>
+      </div>
+      <div class="remove-item" @click="removeTodo(index)">
+        &times;
+      </div>
+>>>>>>> Stashed changes
     </div>
   </div>
-  
 </template>
 
 <script>
-
 export default {
-  name: 'Todo',
-  props: {
-    msg: String
-  },
   data (){
     return {
       newTodo: '',
@@ -49,16 +54,83 @@ export default {
 
       this.newTodo = ''
       this.idForTodo++
+<<<<<<< Updated upstream
+=======
+    },
+    editTodo(todo){
+        this.beforeEditCache = todo.title
+        todo.editing = true
+    },
+    doneEdit(todo){
+      if(todo.title.trim() == ''){
+          todo.title = this.beforeEditCache
+        }
+      todo.editing = false
+    },
+    removeTodo(index){
+      this.todos.splice(index, 1)
+    }, 
+    cancelEdit(todo){
+      todo.title = this.beforeEditCache
+      todo.editing = false
+>>>>>>> Stashed changes
     }
   }
 }
 
 </script>
+
 <style scoped>
-  .list{
-    align-content: center;
+
+
+.todo{
+  margin-left: 75vh;
+  margin-right: 85vh;
+}
+  .todo-input{
+    width: 100%;
     padding: 10px 18px;
     font-size: 18px;
     margin-bottom: 16px;
   }
+<<<<<<< Updated upstream
+=======
+
+.todo-item{
+  margin-bottom: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.remove-item{
+  cursor: pointer;
+  margin-left: 14px ;
+}
+
+.todo-item-left{
+  display: flex;
+  align-items: center;
+}
+
+  .todo-item-label{
+    padding: 10px;
+    border: 1px solid white;
+    margin-left: 12px;
+  }
+
+  .todo-item-edit{
+    font-size: 18px;
+    color: #2c3e50;
+    margin-left: 10px;
+    width: 30vw;
+    padding: 10px;
+    border: 1px solid #ccc;
+    font-family: 'Times New Roman';
+  }
+
+  :focus{
+    outline: none;
+  }
+>>>>>>> Stashed changes
 </style>
